3a8dcb71e32895a5f4b28454c3d87dc719d75c48
fujie <timemeansalot@gmail.com>
2022-04-04 07:12:35 +0800

    learn 01 knapsack

--- dp/test.cpp
+++ dp/test.cpp
@@ -5,7 +5,17 @@
 
 int main()
 {
-
-    std::cout<<"hello world"<<std::endl;
+    std::array<int, 10> value;
+    std::cout << value.size() << std::endl;
+    for (int i = 0; i < 10; i++)
+    {
+        value.at(i) = i;
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << value.at(i) << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "hello world" << std::endl;
     return 0;
 }
