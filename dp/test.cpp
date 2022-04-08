c186d8611f4fee9580afc97b71e1fd11ab42b807
fujie <timemeansalot@gmail.com>
2022-04-08 20:13:26 +0800

    init vector with space and item

--- dp/test.cpp
+++ dp/test.cpp
@@ -6,7 +6,15 @@ using namespace std;
 int main()
 {
     vector<int> v(10, 1); // init a vector which has 10 items, each item is 1
-    for (auto i : v)
-        cout << i << " ";
+    int n = 5, capacity = 10;
+    std::vector<std::vector<int>> maxValue(n + 1, std::vector<int>(capacity + 1, 0));
+    for (auto i : maxValue)
+    {
+        for (auto j : i)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
