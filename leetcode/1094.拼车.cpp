8fd19dba7809f6f11380111c1e6668f904748959
fujie <timemeansalot@gmail.com>
2022-04-21 16:43:40 +0800

    1094 array difference

--- leetcode/1094.拼车.cpp
+++ leetcode/1094.拼车.cpp
@@ -1,13 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=1094 lang=cpp
+ *
+ * [1094] 拼车
+ */
 #include "headfile.h"
+// @lc code=start
 class Solution
 {
 public:
     bool carPooling(vector<vector<int>> &trips, int capacity)
     {
 
-        vector<int> diff(10);
-        vector<int> nums(10);
+        vector<int> diff(1001);
+        vector<int> nums(1001);
 
+        
         // 构造diff数组
         for (int i = 0; i < trips.size(); i++)
         {
@@ -19,10 +26,10 @@ public:
             if (trips[i][2] < diff.size())
                 diff[trips[i][2]-1] += trips[i][0];
 
-            // print diff
-            for (auto d : diff)
-                cout << d << " ";
-            cout << endl;
+            // // print diff
+            // for (auto d : diff)
+            //     cout << d << " ";
+            // cout << endl;
         }
 
         // 计算nums数组
@@ -36,16 +43,4 @@ public:
         return true;
     }
 };
-
-int main()
-{
-
-    vector<vector<int>> bookings{{3,5,9}, {4,2,5},{3,4,6},{9,1,4},{5,6,8},{5,4,6}};
-    int n = 14;
-
-    Solution s;
-
-    bool p = s.carPooling(bookings, n);
-    cout << p << endl;
-    return 0;
-}
+// @lc code=end
