f89f9df98c09400a945ff4508e90b4f56a839686
fujie <timemeansalot@gmail.com>
2022-04-21 19:51:40 +0800

    142: find the first node into circle in list if list has a circle

---
+++ leetcode/142.环形链表-ii.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=cpp
+ *
+ * [142] 环形链表 II
+ */
+#include "headfile.h"
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+
+public:
+    ListNode *detectCycle(ListNode *head)
+    {
+        
+    };
+    // @lc code=end
+
+    /**
+     * 1. what is exactly you need in your job
+     * 2. immediate resource
+     * 3. Priority resource
+     *
+     */
