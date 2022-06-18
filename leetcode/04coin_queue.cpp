26c35f810d91a5e177032d6e50b9d41bdb47ee00
timemeansalot <timemeansalot@gmail.com>
2022-06-18 21:08:46 +0800

    finish problem 4

--- leetcode/04coin_queue.cpp
+++ leetcode/04coin_queue.cpp
@@ -1,65 +1,69 @@
-#include <iostream>
 #include <cstring>
 #include <cstdlib>
+#include <stdio.h>
 using namespace std;
-struct lin
+struct coins
 {
-	int a;
-	int b;
+	int value;
+	int account;
 } s[30];
-int cmp(const void *x, const void *y)
+int compare_function(const void *x, const void *y)
 {
-	struct lin *x1 = (lin *)x;
-	struct lin *x2 = (lin *)y;
-	return x1->a - x2->a;
+	struct coins *x1 = (coins *)x;
+	struct coins *x2 = (coins *)y;
+	return x1->value - x2->value;
 }
 int main()
 {
-	int i, N, C, lin = 0, sum = 0;
-	cin >> N >> C;
+	int i, N, C, small_coins = 0, sum = 0; // small_coins means the account of coin whose value < C
+	scanf("%d %d", &N, &C);
 	for (i = 0; i < N; i++)
 	{
-		int l1, l2;
-		cin >> l1 >> l2;
-		if (l1 >= C)
-			sum += l2;
+		int coin_value, coin_account;
+		
+		// input coin value and account
+		scanf("%d %d", &coin_value, &coin_account);
+		if (coin_value >= C)
+			sum += coin_account;
 		else
 		{
-			s[lin].a = l1, s[lin].b = l2;
-			lin++;
+			s[small_coins].value = coin_value, s[small_coins].account = coin_account;
+			small_coins++;
 		}
 	}
-	qsort(s, lin, sizeof(s[0]), cmp);
+	qsort(s, small_coins, sizeof(s[0]), compare_function);
 	while (1)
 	{
 		int x = C;
-		for (i = lin - 1; i >= 0; i--)
+		for (i = small_coins - 1; i >= 0; i--)
 		{
-			if (s[i].b > 0 && x >= s[i].a)
+			if (s[i].account > 0 && x >= s[i].value)
 			{
-				for (; s[i].b > 0 && x >= s[i].a;)
+				for (; s[i].account > 0 && x >= s[i].value;)
 				{
-					x -= s[i].a;
-					s[i].b--;
+					x -= s[i].value;
+					s[i].account--;
 				}
 			}
-			if(x==0)
-			continue;
+			if (x == 0)
+				continue;
 		}
 		if (x > 0)
-			for (i = 0; i < lin; i++)
+		{
+			for (i = 0; i < small_coins; i++)
 			{
-				if (s[i].b > 0 && x <= s[i].a)
+				if (s[i].account > 0 && x <= s[i].value)
 				{
-					x -= s[i].a;
-					s[i].b--;
+					x -= s[i].value;
+					s[i].account--;
 					break;
 				}
 			}
+		}
 		if (x > 0)
 			break;
 		sum++;
 	}
-	cout << sum;
+	printf("%d\n", sum);
 	return 0;
 }
